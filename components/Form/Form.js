98daab8d58f9f7a0fc751d9c3fa2c7fe7676d5a2
zsh (curl -s https://raw.githubusercontent.com/neuefische/web-setup/main/setup-git <bennet.kutsch@web.de>
2023-07-13 17:22:56 +0200

    form-container

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,4 +1,32 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const FormContainer = styled.div`
+  max-width: 400px;
+  margin: 0 auto;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+`;
+
+const Label = styled.label`
+  margin-bottom: 10px;
+`;
+
+const CheckboxContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-bottom: 10px;
+`;
+
+const QualityContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
 
 export default function MindfulForm() {
   const [location, setLocation] = useState("");
@@ -17,9 +45,9 @@ export default function MindfulForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>
+    <FormContainer>
+      <StyledForm onSubmit={handleSubmit}>
+        <Label>
           Location:
           <input
             type="text"
@@ -27,77 +55,75 @@ export default function MindfulForm() {
             onChange={(e) => setLocation(e.target.value)}
             required
           />
-        </label>
-      </div>
-
-      <div>
-        <label>
-          Nature
-          <input
-            type="checkbox"
-            checked={nature}
-            onChange={(e) => setNature(e.target.checked)}
-          />
-        </label>
-        <label>
-          City
-          <input
-            type="checkbox"
-            checked={city}
-            onChange={(e) => setCity(e.target.checked)}
-          />
-        </label>
-        <label>
-          Loud
-          <input
-            type="checkbox"
-            checked={loud}
-            onChange={(e) => setLoud(e.target.checked)}
-          />
-        </label>
-        <label>
-          Calm
-          <input
-            type="checkbox"
-            checked={calm}
-            onChange={(e) => setCalm(e.target.checked)}
-          />
-        </label>
-        <label>
-          Crowdy
-          <input
-            type="checkbox"
-            checked={crowdy}
-            onChange={(e) => setCrowdy(e.target.checked)}
-          />
-        </label>
-        <label>
-          Clean
-          <input
-            type="checkbox"
-            checked={clean}
-            onChange={(e) => setClean(e.target.checked)}
-          />
-        </label>
-      </div>
-
-      <div>
-        <label>
-          Quality:
-          <input
-            type="range"
-            min={1}
-            max={10}
-            value={quality}
-            onChange={(e) => setQuality(parseInt(e.target.value))}
-          />
-        </label>
-        <span>{quality}</span>
-      </div>
-
-      <div>
-        <button type="submit">Create Mindful Spot</button>
-      </div>
-    </form>
+        </Label>
+        <Label>Filters:</Label>
+        <CheckboxContainer>
+          <label>
+            Nature
+            <input
+              type="checkbox"
+              checked={nature}
+              onChange={(e) => setNature(e.target.checked)}
+            />
+          </label>
+          <label>
+            City
+            <input
+              type="checkbox"
+              checked={city}
+              onChange={(e) => setCity(e.target.checked)}
+            />
+          </label>
+          <label>
+            Loud
+            <input
+              type="checkbox"
+              checked={loud}
+              onChange={(e) => setLoud(e.target.checked)}
+            />
+          </label>
+          <label>
+            Calm
+            <input
+              type="checkbox"
+              checked={calm}
+              onChange={(e) => setCalm(e.target.checked)}
+            />
+          </label>
+          <label>
+            Crowdy
+            <input
+              type="checkbox"
+              checked={crowdy}
+              onChange={(e) => setCrowdy(e.target.checked)}
+            />
+          </label>
+          <label>
+            Clean
+            <input
+              type="checkbox"
+              checked={clean}
+              onChange={(e) => setClean(e.target.checked)}
+            />
+          </label>
+        </CheckboxContainer>
+        <QualityContainer>
+          <Label>
+            Quality:
+            <input
+              type="range"
+              min={1}
+              max={10}
+              value={quality}
+              onChange={(e) => setQuality(parseInt(e.target.value))}
+            />
+          </Label>
+          <span>{quality}</span>
+        </QualityContainer>
+        <div>
+          <button type="submit">Create Mindful Spot</button>
+        </div>
+      </StyledForm>
+    </FormContainer>
   );
 }
